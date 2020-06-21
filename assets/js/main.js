2f0b7e9577541c0d5f5dcb428bd8e7dd8763fc3d
race <racechao@gmail.com>
2020-06-21 17:14:04 +0800

    Toggle comments theme when  theme changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,12 +15,21 @@
   };
 
   const toggleTheme = (state) => {
+    var utterances = document.querySelector('iframe');
     if (state === "dark") {
       localStorage.setItem("theme", "light");
       body.removeAttribute("data-theme");
+      utterances.contentWindow.postMessage(
+        {type: 'set-theme',theme: 'github-light'}, 
+        'https://utteranc.es'
+      );
     } else if (state === "light") {
       localStorage.setItem("theme", "dark");
       body.setAttribute("data-theme", "dark");
+      utterances.contentWindow.postMessage(
+        {type: 'set-theme',theme: 'github-dark'}, 
+        'https://utteranc.es'
+      );
     } else {
       initTheme(state);
     }
